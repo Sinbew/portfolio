00bca23445c8c74487c8d2d9b774dfaf8380ca66
Aleksandr Raskulov <alexandr1991r@gmail.com>
2022-07-13 11:05:29 +0300

    Summary commit / no responsive

--- js/index.js
+++ js/index.js
@@ -1,4 +1,45 @@
-/*------------ About Tabs -----------*/
+//------ Toggle navbar ------//
+const navToggler = document.querySelector(".nav-toggler");
+
+navToggler.addEventListener("click", () => {
+  hideSection();
+  toggleNavbar();
+
+  document.body.classList.toggle("hide-scrolling");
+});
+
+function hideSection() {
+  document.querySelector("section.active").classList.toggle("fade-out");
+}
+
+function toggleNavbar() {
+  document.querySelector(".header").classList.toggle("active");
+}
+
+//---------- Active Section ---------//
+document.addEventListener("click", (event) => {
+  if (event.target.classList.contains("link-item") && event.target.hash !== "") {
+    //activate the overlay to prevent multiplie clicks
+
+    document.querySelector(".overlay").classList.add("active");
+    navToggler.classList.add("hide");
+    if (event.target.classList.contains("nav-item")) {
+      toggleNavbar();
+    } else {
+      hideSection();
+      document.body.classList.add("hide-scrolling");
+    }
+    setTimeout(() => {
+      document.querySelector("section.active").classList.remove("active", "fade-out");
+      document.querySelector(event.target.hash).classList.add("active");
+      window.scrollTo(0, 0);
+      document.body.classList.remove("hide-scrolling");
+      navToggler.classList.remove("hide");
+      document.querySelector(".overlay").classList.remove("active");
+    }, 500);
+  }
+});
+//------------ About Tabs -----------//
 
 const tabsContainer = document.querySelector(".about-tabs");
 const aboutSection = document.querySelector(".about-section");
@@ -14,4 +55,53 @@ function OnTabClick(event) {
     aboutSection.querySelector(".tab-content.active").classList.remove("active");
     aboutSection.querySelector(target).classList.add("active");
   }
+}
+
+/*-------------- Portfolio Item Details Popup ---------------*/
+
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("view-project-btn")) {
+    togglePortfolioPopup();
+    document.querySelector(".portfolio-popup").scrollTo(0, 0);
+    portfolioItemDetails(e.target.parentElement);
+  }
+});
+
+function togglePortfolioPopup() {
+  document.querySelector(".portfolio-popup").classList.toggle("open");
+  document.body.classList.toggle("hide-scrolling");
+
+  document.querySelector(".main").classList.toggle("fade-out");
+}
+
+document.querySelector(".pp-close").addEventListener("click", togglePortfolioPopup);
+
+// Прячем Pop-up при клике вне и по нажатию на ESC
+
+document.addEventListener("click", (event) => {
+  if (event.target.classList.contains("pp-inner")) {
+    togglePortfolioPopup();
+  }
+
+  document.addEventListener("keydown", onEscape);
+
+  function onEscape(event) {
+    if (event.keyCode === 27) {
+      document.querySelector(".portfolio-popup").classList.remove("open");
+      document.querySelector(".main").classList.remove("fade-out");
+      document.body.classList.remove("hide-scrolling");
+      document.removeEventListener("keydown", onEscape);
+      return;
+    }
+  }
+});
+
+// Прячем Pop-up при клике вне и по нажатию на ESC
+
+function portfolioItemDetails(portfolioItem) {
+  document.querySelector(".pp-thumbnail img").src = portfolioItem.querySelector(".portfolio-item-thumbnail img").src;
+
+  document.querySelector(".pp-header h3").innerHTML = portfolioItem.querySelector(".portfolio-item-title").innerHTML;
+
+  document.querySelector(".pp-body").innerHTML = portfolioItem.querySelector(".portfolio-item-details").innerHTML;
 }
